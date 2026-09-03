da17150a4d46d566d04e3fc4eb7baca4ac0bd500
OliverBR1 <Br.0liveira@outlook.com>
2026-09-03 20:04:08 -0300

    create services

---
+++ src/main/java/oliver/tech/smartstock/service/SmartStockService.java
@@ -0,0 +1,27 @@
+package oliver.tech.smartstock.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SmartStockService {
+
+    private final ReportService reportService;
+
+    public SmartStockService(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    public void process(String reportPath){
+
+        try{
+            var items = reportService.readStockReport(reportPath);
+        }   catch (IOException e) {
+                throw new RuntimeException(e);
+        }
+    }
+
+    public void start(String s) {
+    }
+}
