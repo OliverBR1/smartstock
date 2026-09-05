464686cbbc0a4739a0f278ae79bcfbcc1ead7f2c
OliverBR1 <Br.0liveira@outlook.com>
2026-09-05 02:42:14 -0300

    feat SmartstockApplication

--- src/main/java/oliver/tech/smartstock/SmartstockApplication.java
+++ src/main/java/oliver/tech/smartstock/SmartstockApplication.java
@@ -1,13 +1,27 @@
 package oliver.tech.smartstock;
 
+import oliver.tech.smartstock.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class SmartstockApplication {
+@EnableFeignClients
+public class SmartstockApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartstockApplication.class, args);
+	}
+
+	@Autowired
+	private AppConfig appConfig;
+
+	@Override
+	public void run(String ... args) throws Exception {
+		appConfig.getClientId();
+		appConfig.getClientSecret();
 	}
 
 }
