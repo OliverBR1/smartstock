023c9af179b6f7928c5da87e80b65a706ff9b2e4
OliverBR1 <Br.0liveira@outlook.com>
2026-09-05 02:43:26 -0300

    create services

---
+++ src/main/java/oliver/tech/smartstock/service/AuthService.java
@@ -0,0 +1,53 @@
+package oliver.tech.smartstock.service;
+
+import oliver.tech.smartstock.client.AuthClient;
+import oliver.tech.smartstock.client.dto.AuthRequest;
+import oliver.tech.smartstock.config.AppConfig;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AuthService {
+
+    private static final String GRANT_TYPE = "clients_credentials";
+    private static String token;
+    private static LocalDateTime expiresIn;
+
+    private final AuthClient authClient;
+    private final AppConfig appConfig;
+
+    public AuthService(AuthClient authClient,
+                       AppConfig appConfig){
+        this.authClient = authClient;
+        this.appConfig = appConfig;
+    }
+
+    public String getToken() {
+        if (token == null) {
+            generateToken();
+        } else if (expiresIn.isBefore(LocalDateTime.now())) {
+        generateToken();
+        }
+
+        return token;
+    }
+
+    private void generateToken() {
+
+        var request = new AuthRequest(
+                GRANT_TYPE,
+                appConfig.getClientId(),
+                appConfig.getClientSecret()
+        );
+
+        var response = authClient.authenticate(request);
+
+        if(!response.getStatusCode().is2xxSuccessful()) {
+
+        }
+
+        token = "";
+        expiresIn = null;
+    }
+}
---
+++ src/main/java/oliver/tech/smartstock/service/PurchaseSectorService.java
@@ -0,0 +1,15 @@
+package oliver.tech.smartstock.service;
+
+import oliver.tech.smartstock.domain.CsvStockItem;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseSectorService {
+
+    public boolean sendPurchaseRequest(CsvStockItem item, Integer purchaseQuantity) {
+
+
+
+        return false;
+    }
+}
