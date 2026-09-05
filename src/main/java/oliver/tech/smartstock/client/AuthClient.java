00039c9d2aa85306aff308c9c60c948b6af335d1
OliverBR1 <Br.0liveira@outlook.com>
2026-09-05 02:43:52 -0300

    create AppConfig/AuthClient

---
+++ src/main/java/oliver/tech/smartstock/client/AuthClient.java
@@ -0,0 +1,15 @@
+package oliver.tech.smartstock.client;
+
+import oliver.tech.smartstock.client.dto.AuthRequest;
+import oliver.tech.smartstock.client.dto.AuthResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "AuthClient", url = "${api.auth-url}")
+public interface AuthClient {
+
+    @PostMapping(path = "/api/token")
+    ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request);
+}
