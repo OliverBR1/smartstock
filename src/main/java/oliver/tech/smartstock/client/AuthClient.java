a75496836c31b6bcba8586bf49c1c23ff30e6d2b
OliverBR1 <Br.0liveira@outlook.com>
2026-09-05 13:54:28 -0300

    refactor application.properties/ AuthClient

--- src/main/java/oliver/tech/smartstock/client/AuthClient.java
+++ src/main/java/oliver/tech/smartstock/client/AuthClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "AuthClient", url = "${api.auth-url}")
+@FeignClient(name = "AuthClient", url = "${api.client-url}")
 public interface AuthClient {
 
     @PostMapping(path = "/api/token")
