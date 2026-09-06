d60ab908a5e08333cbc4eb2fde82a5173a9521a8
OliverBR1 <Br.0liveira@outlook.com>
2026-09-05 22:51:35 -0300

    new functionality SmartStockService

--- src/main/java/oliver/tech/smartstock/service/SmartStockService.java
+++ src/main/java/oliver/tech/smartstock/service/SmartStockService.java
@@ -1,20 +1,26 @@
 package oliver.tech.smartstock.service;
 
+import oliver.tech.smartstock.client.dto.PurchaseRequest;
 import oliver.tech.smartstock.domain.CsvStockItem;
+import oliver.tech.smartstock.entity.PurchaseRequestEntity;
+import oliver.tech.smartstock.repository.PurchaseRequestRepository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 public class SmartStockService {
 
     private final ReportService reportService;
     private final PurchaseSectorService purchaseSectorService;
+    private final PurchaseRequestRepository repository;
 
     public SmartStockService(ReportService reportService,
-                             PurchaseSectorService purchaseSectorService) {
+                             PurchaseSectorService purchaseSectorService, PurchaseRequestRepository repository) {
         this.reportService = reportService;
         this.purchaseSectorService = purchaseSectorService;
+        this.repository = repository;
     }
 
     public void start(String reportPath){
@@ -27,12 +33,35 @@ public class SmartStockService {
                 if(item.getQuantity() < item.getReorderThreshold()) {
                     var reorderQuantity = calculateReorderQuantity(item);
 
-                    purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
+                   var purchasedWithSuccess = purchaseSectorService.sendPurchaseRequest(item, reorderQuantity);
+
+                    persist(item, reorderQuantity, purchasedWithSuccess);
                 }
             });
         }   catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void persist(CsvStockItem item,
+                         Integer reorderQuantity,
+                         boolean purchasedWithSuccess) {
+
+        var entity = new PurchaseRequestEntity();
+        entity.setItemId(item.getItemId());
+        entity.setItemName(item.getItemName());
+        entity.setQuantityOnStock(item.getQuantity());
+        entity.setReorderThreshold(item.getReorderThreshold());
+        entity.setSupplierName(item.getSupplierName());
+        entity.setSupplierEmail(item.getSupplierEmail());
+        entity.setLastStockUpdateTime(LocalDateTime.parse(item.getLastStockUpdateItem()));
+
+        entity.setPurchaseQuantity(reorderQuantity);
+        entity.setPurchasedWithSuccess(purchasedWithSuccess);
+        entity.setPurchaseDateTime(LocalDateTime.now());
+
+        repository.save(entity);
+
     }
 
     private Integer calculateReorderQuantity(CsvStockItem item) {
